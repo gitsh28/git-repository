76944b704779d3dad19839547d68da8458201e45
gitsh28 <60899613+gitsh28@users.noreply.github.com>
2020-04-21 10:15:34 -0500

    Update program.cs

--- program.cs
+++ program.cs
@@ -2,7 +2,12 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
   
-    Console.WriteLine ("program?")
+    Console.WriteLine ("Program?");
+    int newInt;
+    Console.WriteLine ("Choose what you want to store in the newInt variable [int]");
+    newInt = Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine ("You chose:" + newInt + ".");
+    
   
   }
 
